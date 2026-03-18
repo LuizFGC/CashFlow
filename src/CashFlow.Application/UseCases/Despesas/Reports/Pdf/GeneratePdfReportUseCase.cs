cf66eff76fbacb83bc162279104532235b936150
Luiz Filipe <luizfkz3@gmail.com>
2026-03-18 11:15:27 -0300

    Finalizando renderizacao do pdf

--- src/CashFlow.Application/UseCases/Despesas/Reports/Pdf/GeneratePdfReportUseCase.cs
+++ src/CashFlow.Application/UseCases/Despesas/Reports/Pdf/GeneratePdfReportUseCase.cs
@@ -1,5 +1,7 @@
 ﻿using CashFlow.Application.UseCases.Despesas.Reports.Pdf.Fonts;
 using CashFlow.Domain.Repositories.Despesas;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Despesas.Reports.Pdf;
@@ -21,6 +23,65 @@ public class GeneratePdfReportUseCase: IGeneratePdfReportUseCase
         {
             return [];
         }
-        return [];
+
+        var document = CreateDocument(month);
+        var page = CreatePage(document);
+
+        var paragraph = page.AddParagraph();
+        
+       var title = string.Format( $"Total Gasto em {month.ToString("Y")}");
+       
+       paragraph.AddFormattedText(title, new Font{Name = FontHelper.RALEWAY_REGULAR, Size = 15});
+       
+       paragraph.AddLineBreak();
+
+       var totalDespesas = despesas.Sum(despesa => despesa.Valor);
+       
+       paragraph.AddFormattedText($"R$ {totalDespesas}", new Font{Name = FontHelper.WORKSANS_BLACK, Size = 50});
+        
+        return RenderDocument(document);
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        
+        document.Info.Title = $"Despesas {month.ToString("MMMM")}";
+        document.Info.Author = "Luiz Filipe";
+        
+        var style = document.Styles["Normal"];
+        style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+        
+        
+        
+        return document;
+    }
+
+    private Section CreatePage(Document document)
+    {
+        var page = document.AddSection();
+        page.PageSetup = document.DefaultPageSetup.Clone();
+        page.PageSetup.PageFormat = PageFormat.A4;
+        page.PageSetup.LeftMargin = 40;
+        page.PageSetup.RightMargin = 40;
+        page.PageSetup.TopMargin = 80;
+        page.PageSetup.BottomMargin = 80;
+        
+        return page;
+    }
+
+    private byte[] RenderDocument(Document document)
+    {
+        var renderer = new PdfDocumentRenderer()
+        {
+            Document = document,
+        };
+            
+            renderer.RenderDocument();
+
+            using var file = new MemoryStream();
+            renderer.PdfDocument.Save(file);
+            
+            return file.ToArray();
     }
 }
